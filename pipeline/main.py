94d97622dc8f0d213277d10e560846a0b1b581b6
Brian Bomfim amaral <brianamaral918@gmail.com>
2021-11-20 02:56:09 -0300

    refactor

--- pipeline/main.py
+++ pipeline/main.py
@@ -6,13 +6,17 @@ from transformations.response_transformations import (
     get_state,
     is_status_ok,
 )
-from transformations.row_transformations import filter_nan, filter_ok_status
+from transformations.row_transformations import (
+    filter_nan,
+    filter_ok_status,
+    filter_boolean_column,
+)
 
 from preprocess_pipelines.pipelines import preprocess_gs1, preprocess_cnpj
 
 if __name__ == "__main__":
 
-    produtos_scaneados = pd.read_csv("data/input/infomix.tsv", sep="\t")
+    scaned_codes_dataframe = pd.read_csv("data/input/infomix.tsv", sep="\t")
 
     gs1_dataframe = pd.read_json("data/input/gs1.jl", lines=True)
     gs1_dataframe = preprocess_gs1(gs1_dataframe)
@@ -20,31 +24,31 @@ if __name__ == "__main__":
     cnpj_dataframe = pd.read_json("data/input/cnpjs_receita_federal.jl", lines=True)
     cnpj_dataframe = preprocess_cnpj(cnpj_dataframe)
 
-    descricoes_externas = pd.read_csv("data/input/descricoes_externas.tsv", sep="\t")
-
-    descricoes_externas = descricoes_externas.loc[
-        descricoes_externas["flag_infoprice"] == True
-    ]
-
-    produtos_validados = produtos_scaneados.merge(gs1_dataframe, on="gtin")
-
-    produtos_validados["cnpj_manufacturer"] = produtos_validados[
-        "cnpj_manufacturer"
-    ].apply(lambda x: int(x))
-
-    produtos_validados = produtos_validados.merge(
-        cnpj_dataframe, on="cnpj_manufacturer"
+    external_descriptions_dataframe = pd.read_csv(
+        "data/input/descricoes_externas.tsv", sep="\t"
     )
 
-    produtos_validados = produtos_validados.merge(
-        descricoes_externas, on="gtin", how="left"
+    external_descriptions_dataframe = filter_boolean_column(
+        dataframe=external_descriptions_dataframe, column="flag_infoprice"
     )
 
-    produtos_validados = produtos_validados[
+    validated_scans = scaned_codes_dataframe.merge(gs1_dataframe, on="gtin")
+
+    validated_scans["cnpj_manufacturer"] = validated_scans["cnpj_manufacturer"].apply(
+        lambda x: int(x)
+    )
+
+    validated_scans = validated_scans.merge(cnpj_dataframe, on="cnpj_manufacturer")
+
+    validated_scans = validated_scans.merge(
+        external_descriptions_dataframe, on="gtin", how="left"
+    )
+
+    validated_scans = validated_scans[
         ["gtin", "cnpj", "razao_social", "cidade", "estado", "category", "description"]
     ]
 
-    produtos_validados.columns = [
+    validated_scans.columns = [
         "gtin",
         "cnpj",
         "razao_social",
@@ -54,6 +58,6 @@ if __name__ == "__main__":
         "descricao",
     ]
 
-    produtos_validados.to_csv(
+    validated_scans.to_csv(
         path_or_buf="data/output/gtins_vendidos.tsv", sep="\t", index=False
     )
--- pipeline/transformations/row_transformations.py
+++ pipeline/transformations/row_transformations.py
@@ -8,3 +8,7 @@ def filter_nan(dataframe: DataFrame, column: str) -> DataFrame:
 def filter_ok_status(dataframe: DataFrame) -> DataFrame:
 
     return dataframe.loc[dataframe["status"] == "OK"]
+
+
+def filter_boolean_column(dataframe: DataFrame, column: str) -> DataFrame:
+    return dataframe.loc[dataframe[column] == True]
